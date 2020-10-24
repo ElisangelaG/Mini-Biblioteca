1595b44ffc9d49edad31afd94491e94a7097428d
otaviomorais <otavio.n.morais@hotmail.com>
2020-10-24 15:05:09 -0300

    init

---
+++ src/app/controllers/UsersController.js
@@ -0,0 +1,15 @@
+import User from "../models/User";
+
+class UsersController {
+  async index(req, res, next) {
+    try {
+      const body = { resp: await User.findAll() };
+
+      return res.json(body);
+    } catch (err) {
+      return next(err);
+    }
+  }
+}
+
+export default new UsersController();
